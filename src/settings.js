6a380df6d69258c3aef770bad1dfec759f08beda
pavelzorin <zorin.pavel@gmail.com>
2020-06-13 20:03:44 +0300

    settings

--- src/settings.js
+++ src/settings.js
@@ -1,7 +1,7 @@
 export default {
-    APP_URL: 'http://localhost',
+    APP_URL: 'https://takeoff-contacts.herokuapp.com',
 
-    API_URL: 'http://localhost:3000',
+    API_URL: 'https://takeoff-contacts.herokuapp.com',
     USERS_PATH: '/users',
     USERS_PATH_LOGIN: '/users/login',
     CONTACTS_PATH: '/contacts',
